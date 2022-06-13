3e8311f6d99b143de7f3eccec595938f8f042003
zennvoan12 <72489361+zennvoan12@users.noreply.github.com>
2022-06-13 19:56:51 +0700

    new dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,7 +15,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-
         return view('home', [
             'title' => 'Home',
             'active' => 'home',
@@ -52,7 +51,6 @@ class HomeController extends Controller
      */
     public function show(Home $home)
     {
-
     }
 
     /**
